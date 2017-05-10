0b0f62e335e4775bc2f32fc7426f426550b4fb1a
Arlen Pan <arlenpan@gmail.com>
2017-05-10 06:54:37 -0700

    modified redis to begin data storage apis

--- app.js
+++ app.js
@@ -7,37 +7,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var hbs = require('hbs');
 var app = express();
-
-// setup REDIS. comment out this block to disable redis =======================
-var redis = require('redis');
-if (process.env.REDISTOGO_URL) {
-    // authenticating redis in production env
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
-    var rclient = redis.createClient(rtg.port, rtg.hostname);
-    rclient.auth(rtg.auth.split(":")[1]);
-} else {
-    var rclient = redis.createClient();
-}
-
-rclient.on('connect', function() {
-    console.log('Redis connected!');
-});
-
-rclient.on("error", function(err) {
-    console.log("Error " + err);
-});
-
-// sample redis commands
-rclient.set("key", "value", redis.print);
-rclient.set("key2", "value2", redis.print);
-rclient.get("key", redis.print);
-rclient.get("key", function(err, res) {
-    console.log("REDIS: retrieving 'key' value: " + res);
-});
-rclient.keys("*", function(err, res) {
-    console.log("REDIS: Retrieving all keys: " + res);
-});
-// =============================================================================
+var rclient = require('./redis');
 
 // force redirect HTTPS
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('../redis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+// redis api for adding data
+router.get('/:userid/list-:id', function(req, res) {
+    redis.set(req.params.userid + "-lists", req.params.id, redis.print);
 });
 
 module.exports = router;
