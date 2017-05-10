be22ff2208da08a7c459c2976d1c84c7b93554f9
Emily Yew <emyew2@gmail.com>
2017-05-10 11:34:21 -0700

    rename page tab title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var redis = require('../redis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'RoadRunner' });
 });
 
 // redis api for adding data
