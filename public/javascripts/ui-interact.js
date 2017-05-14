304af54356aa16941169e09c101de0531bc14ed1
Emily Yew <emyew2@gmail.com>
2017-05-14 02:32:06 -0700

    separate UI interactions depending on pathname

--- public/javascripts/ui-interact.js
+++ public/javascripts/ui-interact.js
@@ -1,15 +1,34 @@
 // this file describes all ui interaction functionality
+var pathname = window.location.pathname;
+var MAP = "/";
+var EXPLORE = "/explore";
 
-//make list able to be reordered by dragging
-var elem = document.getElementById("waypoints");
-var editableList = Sortable.create(elem, {
-  filter: '.remove-point',
-  onFilter: function (evt) {
-    var el = editableList.closest(evt.item); // get dragged item
-    el && el.parentNode.removeChild(el);
-  }
-});
+//behavior exclusive to Map page
+if (pathname == MAP) {
+  //make list able to be reordered by dragging
+  var elem = document.getElementById("waypoints");
+  var editableList = Sortable.create(elem, {
+    filter: '.remove-point',
+    onFilter: function (evt) {
+      var el = editableList.closest(evt.item); // get dragged item
+      el && el.parentNode.removeChild(el);
+    }
+  });
 
+  //toggle sidemenu
+  $("#side-menu-toggle-wrapper").click(function () {
+    if($("#side-menu").hasClass("show")) {
+      $("#side-menu").animate({"margin-top": "200%"}, 300).removeClass("show");
+      $(".fa-caret-square-o-down").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
+    } else {
+      $("#side-menu").animate({"margin-top": 0}, 300).addClass("show");
+      $(".fa-caret-square-o-up").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+    }
+  });
+
+}
+
+/*********** Navbar Functionality ************/
 //modal functionality (show signin modal, show register modal, close modal)
 $(".pop-signin").click(function () {
   $(".register").hide();
@@ -22,12 +41,10 @@ $(".pop-register").click(function () {
 $(".close-link").click(function () {
   $(".modal").hide();
 });
-
 //prevent clicks on modal window from triggering modal close
 $(".popup-content").click(function (e) {
   e.stopPropagation();
 });
-
 //toggle navigation
 $(".navbar-menu").click(function () {
   if($(".navbar-lists").hasClass("hide")) {
@@ -37,27 +54,21 @@ $(".navbar-menu").click(function () {
   }
 });
 
-//toggle sidemenu
-$("#side-menu-toggle-wrapper").click(function () {
-  if($("#side-menu").hasClass("show")) {
-    $("#side-menu").animate({"margin-top": "200%"}, 300).removeClass("show");
-    $(".fa-caret-square-o-down").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
-  } else {
-    $("#side-menu").animate({"margin-top": 0}, 300).addClass("show");
-    $(".fa-caret-square-o-up").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
-  }
-});
 
 //reset window if resized to undo toggled states and changes
 $(window).resize(function() {
   if($(window).width() > 750) {
-    $("#side-menu").css({"margin-top": 0}).addClass("show");
-    $(".fa-caret-square-o-up").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
     $(".navbar-lists").css({height: 0}, 200).addClass("hide");
+    if(pathname == MAP) {
+      $("#side-menu").css({"margin-top": 0}).addClass("show");
+      $(".fa-caret-square-o-up").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+    }
   }
 });
 
 $(window).bind("load", function() {
   $(".loader").hide();
-  $("#map").animate({opacity:1}, 300);
+  if(pathname == MAP) {
+    $("#map").animate({opacity:1}, 300);
+  }
 });
