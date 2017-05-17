517588a6fee7cd9cf2b8139f2ddc88d7cee28d3a
Mohsin Ahmed <m8ahmed@ucsd.edu>
2017-05-17 01:10:00 -0700

    fixed create page title

--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET create page */
 router.get('/', function(req, res, next) {
-    res.render('create', { title: 'create' });
+    res.render('create', { title: 'Create' });
 });
 
 module.exports = router;
