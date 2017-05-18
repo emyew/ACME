c7faebc3bdb99f4c49b5b7aff83c1b48ba975f80
Christy Wong <cww016@ucsd.edu>
2017-05-17 20:43:56 -0700

    Added fields to list.

--- config/model_list.js
+++ config/model_list.js
@@ -1,7 +1,14 @@
 var mongoose = require('./mongoose');
 
 var listSchema = mongoose.Schema({
-	locations: []
+	title: String,
+	description: String,
+	dateCreated: Date,
+	dateUpdated: Date,
+	author: String,
+	locations: [],
+	tags: [],
+	listID: Schema.Types.ObjectId
 });
 
 module.exports = mongoose.model('List', listSchema);
